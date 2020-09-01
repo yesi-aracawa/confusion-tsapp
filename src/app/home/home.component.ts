b1e67afb0ccf738c645738688e87166364377860
yesi.servi <yesica.serna@softtek.com>
2020-09-01 14:45:05 -0500

    rx observables

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -24,12 +24,12 @@ export class HomeComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    this.dishservice.getFeaturedDish().then((dish) => (this.dish = dish));
+    this.dishservice.getFeaturedDish().subscribe((dish) => (this.dish = dish));
     this.promotionservice
       .getFeaturedPromotion()
-      .then((promotion) => (this.promotion = promotion));
+      .subscribe((promotion) => (this.promotion = promotion));
     this.leaderService
       .getFeaturedLeader()
-      .then((leader) => (this.leader = leader));
+      .subscribe((leader) => (this.leader = leader));
   }
 }
