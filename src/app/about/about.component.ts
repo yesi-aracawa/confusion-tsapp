bb28050e8dcdccbb4947f873c8101c7edb3b143d
yesi.servi <yesica.serna@softtek.com>
2020-09-04 22:39:06 -0500

    feat: Connect about component to jsonserver

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { Leader } from '../shared/leader';
 import { LEADERS } from '../shared/leaders';
 import { LeaderService } from '../services/leader.service';
@@ -16,12 +16,17 @@ import { flyInOut, expand } from '../animations/app.animations';
 })
 export class AboutComponent implements OnInit {
   leaders: Leader[] = LEADERS;
+  leaderErrMess: String;
 
-  constructor(private leaderService: LeaderService) {}
+  constructor(
+    private leaderService: LeaderService,
+    @Inject('baseURL') private baseURL
+  ) {}
 
   ngOnInit(): void {
-    this.leaderService
-      .getLeaders()
-      .subscribe((leaders) => (this.leaders = leaders));
+    this.leaderService.getLeaders().subscribe(
+      (leaders) => (this.leaders = leaders),
+      (leaderErrmess) => (this.leaderErrMess = <any>leaderErrmess)
+    );
   }
 }
