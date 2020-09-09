cd3f53148e4f10c443b73dfda120e55f33781964
yesi.servi <yesica.serna@softtek.com>
2020-09-09 12:25:02 -0500

    feat: Make conection between home component and jsonserver

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -23,6 +23,8 @@ export class HomeComponent implements OnInit {
   promotion: Promotion;
   leader: Leader;
   dishErrMess: string;
+  promotionErrMess: string;
+  leaderErrMess: string;
 
   constructor(
     private dishservice: DishService,
@@ -36,11 +38,13 @@ export class HomeComponent implements OnInit {
       (dish) => (this.dish = dish),
       (disherrmess) => (this.dishErrMess = <any>disherrmess)
     );
-    this.promotionservice
-      .getFeaturedPromotion()
-      .subscribe((promotion) => (this.promotion = promotion));
-    this.leaderService
-      .getFeaturedLeader()
-      .subscribe((leader) => (this.leader = leader));
+    this.promotionservice.getFeaturedPromotion().subscribe(
+      (promotion) => (this.promotion = promotion),
+      (promoerrmess) => (this.promotionErrMess = <any>promoerrmess)
+    );
+    this.leaderService.getFeaturedLeader().subscribe(
+      (leader) => (this.leader = leader),
+      (leaderErrmess) => (this.leaderErrMess = <any>leaderErrmess)
+    );
   }
 }
